da7434ebc4137e0a4d524ccfc40cc9223379b5ae
Kazuhiro Hiratsuka <papino0319@gmail.com>
2020-11-02 00:00:13 +0900

    update main.cpp

--- template/main.cpp
+++ template/main.cpp
@@ -1,11 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include <atcoder/all>
-using namespace atcoder;
 
 using ll = long long;
-using P = pair<int,int>;
-
+using P = pair<ll,ll>;
 #define rep(i,n) for (ll i = 0; i < (ll)(n); ++i)
 
 int main() {
